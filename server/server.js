f980a71c2ff42b3724dfb77af5a9e1a340fc64ad
Rajeev Lochan Gangulappa Srinivasulu <rajeev.lochan@SLP160143-Rajeev.local>
2019-01-25 11:49:04 +0530

    ADD GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
     const express = require('express');
     const bodyParser = require('body-parser');
+    const { ObjectID } = require('mongodb');
 
     const { mongoose } = require('./db/mongoose');
     const {Todo} = require('./models/todo');
@@ -31,6 +32,34 @@
         })
     });
 
+
+    app.get('/todos/:id', (req, res) => {
+        //valid id using isValid
+        //respond with 404
+        //send back empty body
+        let id = req.params.id;
+        if (!ObjectID.isValid(id)) {
+         return res.status(404).send();
+        }
+
+        Todo.findById(id).then( (todo) => {
+            if (!todo) {
+                return res.status(404).send();
+            }
+            res.send({todo});
+        }).catch( (error) => {
+            res.status(400).send();
+        })
+
+
+        //findById - query TOdos collection
+        //success
+        //if todo send it back
+        //if no todo send back 404 with empty body
+        //error
+        // 400 - and send empty body back
+
+    });
 
     app.listen(3000, () => {
         console.log('Started on Port 3000');
